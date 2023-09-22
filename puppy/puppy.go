896a7bbc79a299c98fdf17e8b4fd45ec736fb5bd
Eric Maki <Eric.Maki@AmericanWell.com>
2023-09-22 12:09:16 -0700

    sometimes you need grit

--- puppy/puppy.go
+++ puppy/puppy.go
@@ -1,6 +1,10 @@
 package puppy
 
-import "github.com/makikorp/animal/dog"
+import (
+	"fmt"
+
+	"github.com/makikorp/animal/dog"
+)
 
 func Bark() string {
 	return "Woof!"
@@ -16,4 +20,8 @@ func BigBark() string {
 
 func BigBarks() string {
 	return dog.WhenGrownUp(Barks())
+}
+
+func v11() {
+	fmt.Println("this is version 1.1.0")
 }
